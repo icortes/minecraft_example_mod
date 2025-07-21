f34f7ee1b866e647e2bd7448d58eac1cabd51f37
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2025-07-21 00:35:37 -0700

    Add worldgen feature and biome modifier classes
    Introduces ModConfiguredFeatures, ModPlacedFeatures, and ModBiomeModifiers classes for custom world generation and biome modification. Updates ModDatapackProvider to register these new data providers for feature and biome modifier bootstrapping.

--- src/main/java/com/omgisa/examplemod/datagen/ModDatapackProvider.java
+++ src/main/java/com/omgisa/examplemod/datagen/ModDatapackProvider.java
@@ -4,11 +4,15 @@ import com.omgisa.examplemod.ExampleMod;
 import com.omgisa.examplemod.enchantment.ModEnchantments;
 import com.omgisa.examplemod.trim.ModTrimMaterials;
 import com.omgisa.examplemod.trim.ModTrimPatterns;
+import com.omgisa.examplemod.worldgen.ModBiomeModifiers;
+import com.omgisa.examplemod.worldgen.ModConfiguredFeatures;
+import com.omgisa.examplemod.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -17,7 +21,11 @@ public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER =
             new RegistrySetBuilder().add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
                                     .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
-                                    .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+                                    .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap)
+
+                                    .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+                                    .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+                                    .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
