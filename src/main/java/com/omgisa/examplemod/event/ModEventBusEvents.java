17272e4309be23f5e10119e1ef29680c8fa721b4
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2025-08-02 14:13:25 -0700

    Add gecko spawn to swamps and plains biomes
    Introduced a new biome modifier and JSON definition to spawn geckos in swamp and plains biomes. Registered spawn placements for the gecko entity and updated ModBiomeModifiers and ModEventBusEvents accordingly.

--- src/main/java/com/omgisa/examplemod/event/ModEventBusEvents.java
+++ src/main/java/com/omgisa/examplemod/event/ModEventBusEvents.java
@@ -4,10 +4,14 @@ import com.omgisa.examplemod.ExampleMod;
 import com.omgisa.examplemod.entity.ModEntities;
 import com.omgisa.examplemod.entity.client.GeckoModel;
 import com.omgisa.examplemod.entity.custom.GeckoEntity;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 @EventBusSubscriber(modid = ExampleMod.MOD_ID)
 public class ModEventBusEvents {
@@ -20,5 +24,14 @@ public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.GECKO.get(), GeckoEntity.createAttributes().build());
+    }
+
+    @SubscribeEvent
+    public static void registerSpawnPlacements(RegisterSpawnPlacementsEvent event) {
+        event.register(ModEntities.GECKO.get(),
+                       SpawnPlacementTypes.ON_GROUND,
+                       Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                       Animal::checkAnimalSpawnRules,
+                       RegisterSpawnPlacementsEvent.Operation.REPLACE);
     }
 }
